906b91ee0552e5c6e8c7154b56ff375b1cbacf0a
Valentin Zhilko <>
2022-12-03 19:46:48 +0100

    removed extra file

--- feature/list/src/main/java/by/vzhilko/list/domain/dto/ImagePageItemData.kt
+++
@@ -1,8 +0,0 @@
-package by.vzhilko.list.domain.dto
-
-import by.vzhilko.core.dto.ImageData
-
-sealed class ImagePageItemData {
-    data class Data(val imageData: ImageData) : ImagePageItemData()
-    data class Separator(val description: String) : ImagePageItemData()
-}
